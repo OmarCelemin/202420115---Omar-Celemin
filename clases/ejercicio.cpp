849c05632810ad06cc2169f8985a044a2d0d8146
OmarCelemin <omar.celemin@uptc.edu.co>
2025-11-13 15:57:33 +0000

    WTEREFDSFED

--- clases/ejercicio.cpp
+++ clases/ejercicio.cpp
@@ -1,92 +1,203 @@
-#include<iostream>
-
+#include "iostream"
 using namespace std;
 
-class TitlesManager {
-    public:
-        bool isRunning = true;
-        string* titles;
-        int size;
+class SalesManager
+{
 
-        void run() {
-            while(isRunning) {
-                int userAction = getUserAction();
-                switch (userAction)
+public:
+    int salesSize = 0, userSelection;
+    bool runningStatus = 1;
+
+    int *prices;
+    string *articles;
+    string text;
+
+    // esta funcion gestiona todo, elige que mostar o hacer a partir de las instrucciones del usuario
+    void run()
+    {
+
+        while (runningStatus)
+        {
+
+            userSelection = getUserSelection();
+            switch (userSelection)
             {
+            case 1:
+                cout << "\nDame el numero de ventas a registrar: ";
+                cin >> salesSize;
 
-                case 1:
-                    modifyArray();
-                    break;
-                
-                case 2:
-                    printTitles();
-                    break;
-                   
-                case 3:
-                    titles = newList();
-                    break;
+                getNewSales();
 
-                case 0:
-                    delete[] titles;
-                    isRunning = false;
-                    break;
+                break;
 
-                default:
+            case 2:
+                printSales();
+                break;
 
-                    break;
-                }
+            case 3:
+                modifySale();
+                break;
+
+            case 4:
+                findSale();
+                break;
+
+            case 5:
+                salesInRange();
+                break;
+
+            case 6:
+                calculateTotal();
+                break;
+
+            case 7:
+                delete[] prices;
+                delete[] articles;
+
+                runningStatus = 0;
+                break;
+
+            default:
+                break;
             }
         }
+    }
 
-        string* newList() {
-            cout << "Ingrese el tamaño de la lista: ";
-            cin >> size;
-            string* newTitles = new string[size];
-            for (int i = 0; i < size; i++)
+    // convierte los imputs del usuario a minusculas para hacer un mejor manejo en las funciones secundarias
+    string mayusToMinus()
+    {
+
+        for (int i = 0; i < text.length(); i++)
+        {
+            if ((text[i] >= 'A') && (text[i] <= 'Z'))
             {
-                string newValue;
-                cout << "Ingrese el valor para la posicion " << i + 1 << ": ";
-                cin >> newValue;
-                newTitles[i] = newValue;
-            }
-            return newTitles;
-        }
-
-        void modifyArray() {
-            int positionToModify;
-            printTitles();
-            cout << "Ingrese la posicion a modificar: ";
-            cin >> positionToModify;
-            positionToModify = positionToModify - 1;
-            string newValue;
-            cout << "Ingrese el nuevo valor: ";
-            cin >> newValue;
-            titles[positionToModify] = newValue;
-            printTitles();
-        }
-
-        void printTitles() {
-            cout << "Lista: " << endl;
-            for (int i = 0; i < size; i++)
-            {
-                cout << "Posicion " << i + 1 << " - Valor: " << titles[i] << endl;
+                text[i] += 32;
             }
         }
+        return text;
+    }
 
-        int getUserAction() {
-            int userAction;
-            cout << "Ingrese una opcion: " << endl;
-            cout << "1 para modificar la lista: " << endl;
-            cout << "2 para imprimir la lista: " << endl;
-            cout << "3 para re-inicializar la lista: " << endl;
-            cout << "0 para salir: " << endl;
-            cin >> userAction;
-            return userAction;
+    // obtiene la seleccion del usuario y las envia al switch para procesarlas
+    int getUserSelection()
+    {
+        int userSelection;
+
+        cout << "\nPor favor seleccione:\n\n"
+             << "1. Registrar o reiniciar ventas.\n"
+             << "2. Mostrar todas las ventas.\n"
+             << "3. Modificar una venta.\n"
+             << "4. Buscar una venta.\n"
+             << "5. Mostrar ventas dentro de rango de precios.\n"
+             << "6. Calcular total de ventas.\n"
+             << "7. Salir. \n\n"
+             << "Ingrese una opcion: ";
+
+        cin >> userSelection;
+        return userSelection;
+    }
+
+    // crea las listas de articulos y precios usando punteros
+    void getNewSales()
+    {
+
+        for (int i = 0; i < salesSize; i++)
+        {
+            cout << "\nIngresa el nombre del articulo: " << i + 1 << ": ";
+            cin >> articles[i];
+            text = articles[i];
+            articles[i] = mayusToMinus();
+
+            cout << "\nIngresa el precio del articulo: " << i + 1 << ": ";
+            cin >> prices[i];
         }
+
+        return;
+    }
+
+    // imprime los valores de las listas
+    void printSales()
+    {
+        for (int i = 0; i < salesSize; i++)
+        {
+            cout << "Venta: " << articles[i] << "-> Valor: " << prices[i] << "\n";
+        }
+    }
+
+    // modifica una posicion de las listas especificada por el usuario
+    void modifySale()
+    {
+        int positionToModify, newPrice;
+        string newArticle;
+        cout << "Seleccione la posicion a modificar: ";
+        cin >> positionToModify;
+        positionToModify = positionToModify - 1;
+
+        cout << "Ingrese el nuevo articulo: ";
+        cin >> newArticle;
+        text = newArticle; 
+        newArticle = mayusToMinus();
+        articles[positionToModify] = newArticle;
+
+        cout << "Ingrese el nuevo precio: ";
+        cin >> newPrice;
+        prices[positionToModify] = newPrice;
+
+        printSales();
+    }
+
+    // busca un articulo por su nombre y muestra su precio y la posicion en la que se encuentra
+    void findSale()
+    {
+        string article;
+        cout << "Ingrese el nombre del articulo: ";
+        cin >> article;
+        text = article;
+        article = mayusToMinus();
+
+        for (int i = 0; i < salesSize; i++)
+        {
+            if (articles[i] == article)
+            {
+                cout << "El articulo " << article << " se encuentra en la posicion " << i + 1 << " y su valor es de: " << prices[i] << "\n";
+            }
+        }
+    }
+
+    // muestra los articulos de las listas que se encuentran en un rango de precios
+    void salesInRange()
+    {
+        int minVal, maxVal;
+        cout << "\nIngresa el valor minimo:";
+        cin >> minVal;
+        cout << "\nIngresa el valor maximo:";
+        cin >> maxVal;
+
+        cout << "\nEstos articulos se encuentran en el rango de precios de " << minVal << " a " << maxVal << "\n\n";
+
+        for (int i = 0; i < salesSize; i++)
+        {
+            if ((prices[i] >= minVal) && (prices[i] <= maxVal))
+            {
+                cout << articles[i] << "->" << prices[i] << "\n";
+            }
+        }
+    }
+
+    // muestra el total de las ventas registradas
+    void calculateTotal()
+    {
+        int total = 0;
+        for (int i = 0; i < salesSize; i++)
+        {
+            total += prices[i];
+        }
+        cout << "\nEl total en ventas es: " << total;
+    }
 };
 
-int main() {
-    TitlesManager manager;
-    manager.run();
+int main()
+{
+    SalesManager sales;
+    sales.run();
     return 0;
 }
